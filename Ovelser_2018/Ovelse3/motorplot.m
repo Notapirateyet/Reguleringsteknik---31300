b45bca00dd63e38187a039fbd2284af5fa44226d
s163905 <s163905@student.dtu.dk>
2018-02-14 16:26:52 +0100

    motorplot code

--- Ovelser_2018/Ovelse3/motorplot.m
+++ Ovelser_2018/Ovelse3/motorplot.m
@@ -1,16 +1,21 @@
 %Data
-data = load('xxx.txt');
+data = load('data_sutten.txt');
+% Freja (4)
+%  1    time 0.000 sec
+%  2  3 Motor voltage [V] left, right: 3.00 3.00
+%  4  5 Motor current left, right [A]: 0.150 0.128
+%  6  7 Wheel velocity [m/s] left, right: -0.0000 0.0000
 figure(100) % start figure 100
 yyaxis left % use left y-axis
 hold off    % repaint plot
-plot(data(:,1), model.data(:,2),'-g','linewidth',2) %m/s
+plot(data(:,1), data(:,7),'-g','linewidth',2) %m/s
 hold on  % keep previous plots
-plot(data(:,1), model.data(:,1),'-r','linewidth',2) %A
+plot(data(:,1), data(:,5),'-r','linewidth',2) %A
 ylabel('velocity and current')
 hold on
 yyaxis right
 hold off % repaint plots
-plot(data(:,1), model.data(:,3),'--m','linewidth',2) %V
+plot(data(:,1), data(:,3),'--m','linewidth',2) %V
 axis([0,1,0,5]) % axis limits [x-min, x-max, ymin, ymax]
 ylabel('motorsp?nding');
 %
--- Ovelser_2018/Ovelse3/simplot.m
+++ Ovelser_2018/Ovelse3/simplot.m
@@ -1,19 +1,23 @@
 % run simulink simulation with 1 second duration
-% exports data in a model variablesim(?regbot_x_model?, 1.0)
+% exports data in a model variablesim('regbot_x_model', 1.0)
 % plot data
 figure(100) % start figure 100
 yyaxis left % use left y-axis
 hold off    % repaint plot
-plot(model.time, model.data(:,3),?-g?,?linewidth?,2) %m/s
+plot(model.time, model.data(:,3),'-g','linewidth',2) %m/s
 hold on  % keep previous plots
-plot(model.time, model.data(:,2),?-r?,?linewidth?,2) %A
-ylabel(?velocity and current?)hold onyyaxis righthold off % repaint plots
-plot(model.time, model.data(:,1),?--m?,?linewidth?,2) %V
+plot(model.time, model.data(:,2),'-r','linewidth',2) %A
+ylabel('velocity and current')
+hold on
+yyaxis right
+hold off % repaint plots
+plot(model.time, model.data(:,1),'--m','linewidth',2) %V
 axis([0,1,0,5]) % axis limits [x-min, x-max, ymin, ymax]
-ylabel(?motorsp?nding?)
+ylabel('motorsp?nding')
 %
-legend(?Hastighed [m/s]?, ?Motorstr?m [A]?, ...
-    ?Motorsp?nding [V]?, ?location?,?east?);
-xlabel(?time [s]?)set(gca,?FontSize?,12);
+legend('Hastighed [m/s]', 'Motorstr?m [A]', ...
+    'Motorsp?nding [V]', 'location','east');
+xlabel('time [s]')
+set(gca,'FontSize',12);
 grid on     % show major grid (for left axis)
 grid MINOR  % show also fine grid
