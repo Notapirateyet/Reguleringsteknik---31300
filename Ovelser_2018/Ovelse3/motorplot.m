6e81e5279da8edb2d8736d017a126083fbdd0bcc
Asger <s163933@student.dtu.dk>
2018-02-21 15:59:20 +0100

    Calculated Bm in udregninger.m

--- Ovelser_2018/Ovelse3/motorplot.m
+++ Ovelser_2018/Ovelse3/motorplot.m
@@ -1,10 +1,10 @@
 %Data
-data = load('data_sutten.txt');
+data = load('3_6V.txt');
 % Freja (4)
 %  1    time 0.000 sec
 %  2  3 Motor voltage [V] left, right: 3.00 3.00
-%  4  5 Motor current left, right [A]: 0.150 0.128
-%  6  7 Wheel velocity [m/s] left, right: -0.0000 0.0000
+%  4  5 Motor current left, right [A]: 0.135 0.132
+%  6  7 Wheel velocity [m/s] left, right: 0.0000 0.0000
 figure(99) % start figure 100
 yyaxis left % use left y-axis
 hold off    % repaint plot
@@ -13,10 +13,11 @@ hold on  % keep previous plots
 plot(data(:,1), data(:,5),'-r','linewidth',2) %A
 ylabel('velocity and current')
 hold on
+axis([0,1,-1,10])
 yyaxis right
 hold off % repaint plots
 plot(data(:,1), data(:,3),'--m','linewidth',2) %V
-axis([0,1,0,5]) % axis limits [x-min, x-max, ymin, ymax]
+axis([0,1,-1,10]) % axis limits [x-min, x-max, ymin, ymax]
 ylabel('motorsp?nding');
 %
 legend('Hastighed [m/s]', 'Motorstr?m [A]', 'Motorsp?nding [V]');
--- Ovelser_2018/Ovelse3/udregninger.m
+++ Ovelser_2018/Ovelse3/udregninger.m
@@ -1,34 +1,90 @@
-%Data
-data = load('data_sutten.txt');
+clear;
+%% Data
+% data = load('data_sutten.txt');
 % Freja (4)
 %  1    time 0.000 sec
 %  2  3 Motor voltage [V] left, right: 3.00 3.00
 %  4  5 Motor current left, right [A]: 0.150 0.128
 %  6  7 Wheel velocity [m/s] left, right: -0.0000 0.0000
 
-%%%Udregnign af diverse v?rdier:
-%%motorkonstant Kemf
+data = load('3_6V.txt');
+
+d_start_3v = 250;
+d_end_3v = 460;
+d_start_6v = 800;
+d_end_6v = 970;
+
+%% Udregnign af diverse v?rdier:
+% motorkonstant Kemf
 
 %Va - Vemf - Ra * Ia = 0 <=> Vemf = Va - Ra*Ia
 %Vemf = Kemf * omega <=> kemf = Vemf/omega = Vemf/(v*(G/rw))
 %Vi v?lger Va = 4V, og finder s? motorsp?ndingen i det interval
 
-Ia = (sum(data(206:506,5)))/(300);
+Ia = (sum(data(d_start_3v:d_end_3v,5)))/(d_end_3v - d_start_3v);
 Va = 3; %V
 Ra = 4; %Ohm
 G = 9.68; %gear
 rw = 0.03; %m
 Vemf = Va - Ra*Ia;
 
-omega = ((sum(data(206:506,5)))/(300)) * G/rw;
+omega3v = (sum(data(d_start_3v:d_end_3v,7)))/(d_end_3v - d_start_3v) * G/rw;
 
-Kemf = Vemf/omega;
+Kemf = Vemf/omega3v;
 Ktau = Kemf;
 
-Bm = (Ia*Ktau)/omega;
+% Måske
+Bm = (Ia*Ktau)/omega3v;
 
-Ra = (Va-Vemf)/Ia;
+Ra3v = (Va-Vemf)/Ia;
+
+
+%% Samme men ved 6V
+
+Ia =(sum(data(d_start_6v:d_end_6v,5)))/(d_end_6v - d_start_6v);
+Va = 6; %V
+Ra = 4; %Ohm
+G = 9.68; %gear
+rw = 0.03; %m
+Vemf6v = Va - Ra*Ia;
+
+omega6v = (sum(data(d_start_6v:d_end_6v,7)))/(d_end_6v - d_start_6v) * G/rw;
+
+Kemf6v = Vemf6v/omega6v;
+Ktau6v = Kemf6v;
+
+% Måske
+Bm6v = (Ia*Ktau6v)/omega6v;
+
+Ra6v = (Va-Vemf6v)/Ia;
 
 
 
 
+%% Udregnning af Bm
+
+% G(s) = K_tau/(J_m*s+B_m)
+
+deltaOmega = (((sum(data(d_start_6v:d_end_6v,7)))/(d_end_6v - d_start_6v)) * G/rw)-(((sum(data(d_start_3v:d_end_3v,7)))/(d_end_3v - d_start_3v)) * G/rw);
+deltaV = 6-3;
+deltaI = ((sum(data(d_start_6v:d_end_6v,5)))/(d_end_6v - d_start_6v)) - ((sum(data(d_start_3v:d_end_3v,5)))/(d_end_3v - d_start_3v));
+
+Bm_v1 = deltaI*Ktau/deltaOmega;
+Bm_v2 = -(deltaI*Kemf*Ktau) / (deltaI*Ra-deltaV);
+
+%% Udregning af 
+
+
+
+%% Output
+fprintf("K_emf = %e\nBm_v1 = %e \tBm_v2 = %e\n", [Kemf, Bm_v1,Bm_v2]);
+
+
+
+
+
+
+
+
+
+fprintf("\n");
