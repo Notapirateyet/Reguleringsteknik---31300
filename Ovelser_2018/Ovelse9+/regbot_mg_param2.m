275451926c4a4010fb00b33640a313d087398bfc
Asger <s163933@student.dtu.dk>
2018-04-18 15:44:02 +0200

    Rettet mange fejl

--- Ovelser_2018/Ovelse9+/regbot_mg_param2.m
+++ Ovelser_2018/Ovelse9+/regbot_mg_param2.m
@@ -33,18 +33,19 @@ alpha = 0.3;
 beta = 4;
 
 %% simulering af model i 2 sekunder
-sim('regbot_2mg', 2);
+sim('regbot_3mg', 2);
 %
 %% Plots
 
-% figure(1)
-% plot(speed_out);
-
+figure(1)
+plot(speed_out);
+figure(2)
+plot(pitchout);
 
 %% linearisering i arbejdspunkt (startvinkel)
 startAngle = 30; % in degrees
 % linmod forventer her at model har netop et input og et output
-[A,B,C,D] = linmod('regbot_1mg');
+[A,B,C,D] = linmod('regbot_2mg');
 [num,den] = ss2tf(A,B,C,D);
 % overføringsfunktion fra hastighed til pitch
 Gsp = minreal(tf(num,den))
@@ -52,45 +53,42 @@ Gsp = minreal(tf(num,den))
 %% Datasjov
 close all
 
-Kpp = 1;
-tauip = 0.2;
-Gi = tf([tauip 1],[tauip 0]);
-
-
 figure(2)
-nyquist(Kpp*Gsp*Gi)
-hold on
-nyquist(Gsp*Kpp);
-
-
-%% En Frederegulator
-
-Ni = 5;
-a = 0.01;
-gamma_m = 80;
-phi_i = atan(-1/Ni)*180/pi;
-phi_m = asin((1-a)/(1+a))*180/pi;
--180 - phi_i - phi_m + gamma_m
-
-omega_c = 3.51e3;
-tau_i = Ni/omega_c;
-tau_d = 1/(omega_c*sqrt(a));
-
-Gi = tf([tau_i 1],[tau_i 0]);
-Gd = tf([tau_d 1],[a*tau_d 1]);
-
-[M,P] = bode(Gi*Gd*Gsp,omega_c);
-Kp = 1/M;
-Go = Gi*Gsp*Kp;
-Gc = Go/(1+Gd*Go);
-
-figure(15)
-step(Gc)
-
-figure(16)
-margin(Go)
+nyquist(Gsp);
+figure(3)
+bode(Gsp);
 grid
-figure(17)
-nyquist(Gc)
+figure(4)
+pzmap(Gsp);
 
-stepinfo(Gc)
+%% En cmosgulator
+% Vi har en hump ved 7.06 rad/s
+Ni = 5;
+tau_ib = Ni/7.06;
+gi = tf([tau_ib 1],[tau_ib 0]);
+
+
+alpha_b = 0.01;
+omega_c = 7.06;
+tau_db = 1/(omega_c*sqrt(alpha_b));
+gd = tf([tau_db 1],1);
+[M,P] = bode(Gsp*gi*gd,omega_c);
+Kp_b = -8;
+
+figure(5)
+nyquist(gd*Kp_b*Gsp*gi);
+figure(7)
+bode(Kp_b*Gsp*gi);
+
+Gcl = (Gsp*gi*Kp_b)/(1+gd*Gsp*gi*Kp_b);
+figure(6)
+step(Gcl)
+
+
+
+
+
+
+
+
+
