00a6d9bfa8b552907bfa9772b7d693f503dbcef2
s163905 <s163905@student.dtu.dk>
2018-05-02 17:08:58 +0200

    Essentielt fix i simulink model

--- Ovelser_2018/Ovelse9+/plotting_balance.m
+++ Ovelser_2018/Ovelse9+/plotting_balance.m
@@ -38,3 +38,42 @@ figure(120)
 plot(pitchout.Time,pitchout.Data)
 xlabel('Time [s]')
 ylabel('Tilt [rad]')
+
+%% plot af balance/hastighed regulator - simuleret
+close all
+figure(130)
+yyaxis left
+plot(pitchout.Time,pitchout.Data,'LineWidth',2)
+xlabel('Time [s]','FontSize', 14)
+ylabel('Tilt [rad]','FontSize', 14)
+hold on 
+yyaxis right
+plot(speed_out.Time,speed_out.Data,'LineWidth',2)
+ylabel('Velocity [rad]','FontSize', 14)
+legend('Tilt','Velocity')
+hold off
+
+%% plot af balance/hastighed regulator - logget data
+close all
+%logfile from robot Freja (4)
+% Freja (4)
+%  1    time 0.021 sec
+%  2  3 Wheel velocity [m/s] left, right: 0.4871 0.4185
+%  4  5  6  7 Pose x,y,h,tilt [m,m,rad,rad]: 0.00709941 -6.10527e-06 -0.0026173 0.417936
+
+data = load('velocity_balance_30s.txt');
+figure(140)
+yyaxis left
+plot(data(:,1),data(:,7),'b','LineWidth',2)
+xlabel('Time [s]','FontSize', 14)
+ylabel('Tilt [rad]','FontSize', 14)
+hold on 
+yyaxis right
+plot(data(:,1),data(:,2),'g','LineWidth',2);
+ylabel('Velocity [m/s]','FontSize', 14)
+linewidth = 2;
+hold on 
+plot(data(:,1),data(:,3),'-','LineWidth',1.5);
+legend('Tilt','Left wheel velocity','Right wheel velocity')
+xlim([0 30]);
+hold off
